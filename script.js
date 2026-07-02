e5d29b4a31829015798daad329a72757fa2f5690
Samuel Pereira <75548737+Bifooh@users.noreply.github.com>
2026-07-02 17:34:16 -0600

    Enhance homepage layout and navigation; add email. Made Top nav bar sticky and blurred background

---
+++ script.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        type: "Web App", 
+        title: "Better Future Academy", 
+        description: "Education platform for English learners, mentors, scheduling, and practice tools",
+        tags: ["React", "Node.js", "MySQL"],
+        link: "#"
+    },
+    {
+        type: "Landing Page", 
+        title: "Harmon’s Heating & Cooling", 
+        description: "Modern website for a local HVAC business focused on trust and mobile experience.",
+        tags: ["HTML", "CSS", "JS"],
+        link: "#"
+    },
+    {
+        type: "Game", 
+        title: "Pixel Quest", 
+        description: "Small 2D game experiment built with JavaScript and creative coding ideas..",
+        tags: ["JavaScript", "Canvas", "Games"],
+        link: "#"
+    }
+]
+
